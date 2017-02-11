41059ce75612d99a87a080da4bd4a90ba6d40ced
Jason E. Aten <j.e.aten@gmail.com>
2017-02-10 23:54:43 -0600

    working on read/write deadlines

---
+++ server/lcon/pipe.go
@@ -0,0 +1,144 @@
+// from https://github.com/bradfitz/http2/pull/8/files
+//
+// motivation: https://groups.google.com/forum/#!topic/golang-dev/k0bSal8eDyE
+//
+// Copyright 2014 The Go Authors.
+// See https://code.google.com/p/go/source/browse/CONTRIBUTORS
+// Licensed under the same terms as Go itself:
+// https://code.google.com/p/go/source/browse/LICENSE
+
+// package pipe gives a local in-memory net.Conn
+package lcon
+
+import (
+	"fmt"
+	"net"
+	"sync"
+	"time"
+)
+
+// Pipe is buffered version of net.Pipe. Reads
+// will block until data is available.
+type Pipe struct {
+	b       buffer
+	c       sync.Cond
+	m       sync.Mutex
+	Flushed chan bool
+
+	readDeadline  time.Time
+	writeDeadline time.Time
+}
+
+// NewPipe must be given a buf of
+// pre-allocated size to use as the
+// internal buffer between reads
+// and writes.
+func NewPipe(buf []byte) *Pipe {
+	p := &Pipe{
+		b:       buffer{buf: buf},
+		Flushed: make(chan bool, 1),
+	}
+	p.c = *sync.NewCond(&p.m)
+	return p
+}
+
+var ErrDeadline = fmt.Errorf("deadline exceeded")
+
+// Read waits until data is available and copies bytes
+// from the buffer into p.
+func (r *Pipe) Read(p []byte) (n int, err error) {
+	r.c.L.Lock()
+	defer r.c.L.Unlock()
+	if !r.readDeadline.IsZero() {
+		now := time.Now()
+		dur := r.readDeadline.Sub(now)
+		if dur <= 0 {
+			return 0, ErrDeadline
+		}
+		go func(dur time.Duration) {
+			time.Sleep(dur)
+			r.c.Broadcast()
+		}(dur)
+	}
+	for r.b.Len() == 0 && !r.b.closed {
+		r.c.Wait()
+	}
+	return r.b.Read(p)
+}
+
+// Write copies bytes from p into the buffer and wakes a reader.
+// It is an error to write more data than the buffer can hold.
+func (w *Pipe) Write(p []byte) (n int, err error) {
+	w.c.L.Lock()
+	defer w.c.L.Unlock()
+	if !w.writeDeadline.IsZero() {
+		now := time.Now()
+		dur := w.writeDeadline.Sub(now)
+		if dur <= 0 {
+			return 0, ErrDeadline
+		}
+		go func(dur time.Duration) {
+			time.Sleep(dur)
+			w.c.Broadcast()
+		}(dur)
+	}
+	defer w.c.Signal()
+	defer w.flush()
+	return w.b.Write(p)
+}
+
+var ErrLconPipeClosed = fmt.Errorf("lcon pipe closed")
+
+func (c *Pipe) Close() error {
+	c.SetErrorAndClose(ErrLconPipeClosed)
+	return nil
+}
+
+func (c *Pipe) SetErrorAndClose(err error) {
+	c.c.L.Lock()
+	defer c.c.L.Unlock()
+	defer c.c.Signal()
+	c.b.Close(err)
+}
+
+// Pipe technically fullfills the net.Conn interface
+
+func (c *Pipe) LocalAddr() net.Addr  { return addr{} }
+func (c *Pipe) RemoteAddr() net.Addr { return addr{} }
+
+func (c *Pipe) flush() {
+	if len(c.Flushed) == 0 {
+		c.Flushed <- true
+	}
+}
+
+type addr struct{}
+
+func (a addr) String() string  { return "memory.pipe:0" }
+func (a addr) Network() string { return "in-process-internal" }
+
+// SetDeadline implements the net.Conn method
+func (c *Pipe) SetDeadline(t time.Time) error {
+	err := c.SetReadDeadline(t)
+	err2 := c.SetWriteDeadline(t)
+	if err != nil {
+		return err
+	}
+	return err2
+}
+
+// SetWriteDeadline implements the net.Conn method
+func (c *Pipe) SetWriteDeadline(t time.Time) error {
+	c.c.L.Lock()
+	c.writeDeadline = t
+	c.c.L.Unlock()
+	return nil
+}
+
+// SetReadDeadline implements the net.Conn method
+func (c *Pipe) SetReadDeadline(t time.Time) error {
+	c.c.L.Lock()
+	c.readDeadline = t
+	c.c.L.Unlock()
+	return nil
+}
