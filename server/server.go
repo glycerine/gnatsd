86102e202f59e442bd9b0197bdaa844cfbcc502e
Jason E. Aten <j.e.aten@gmail.com>
2017-02-11 03:11:35 -0600

    InternalClient interface in icli.go

--- server/server.go
+++ server/server.go
@@ -83,53 +83,6 @@ type Server struct {
 	icli          []internalClient // in-process internal clients
 }
 
-// InternalClient provides an interface
-// to internal clients that live
-// in-process with the Server
-// on their own goroutines.
-//
-// An example of an internal client
-// is the health monitoring client.
-// In order to be effective, its lifetime
-// must exactly match that of the
-// server it monitors.
-//
-type InternalClient interface {
-
-	// Start should run the client on
-	// a background goroutine.
-	//
-	// The Server s will invoke Start()
-	// as a part of its own init and setup.
-	//
-	// The s pointer will be passed already locked,
-	// and must not be unlocked during Start().
-	//
-	// The lsnReady channel
-	// will be closed by the Server once the
-	// accept loop has spun up.
-	//
-	// Before exiting, the client should
-	// call back on register(nc)
-	// to provide the server with
-	// a net.Conn for communication.
-	//
-	// Any returned error will be logged and
-	// will prevent the Server from calling
-	// Stop() on termination. If nil is
-	// returned then Stop() should function
-	// correctly and not block.
-	//
-	Start(s *Server, lsnReady chan struct{}, register func(nc net.Conn)) error
-
-	// Stop should shutdown the internal client.
-	// The Server will invoke Stop() as a part
-	// of its own shutdown process, so long as
-	// Start() did not return an error.
-	//
-	Stop()
-}
-
 // Make sure all are 64bits for atomic use
 type stats struct {
 	inMsgs        int64
