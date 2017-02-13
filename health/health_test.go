249cd38682fb4d1ebd165100d785e05b3262b7c4
Jason E. Aten <j.e.aten@gmail.com>
2017-02-13 02:34:47 -0600

    ft 102/103. wip

--- health/health_test.go
+++ health/health_test.go
@@ -542,20 +542,5 @@ func Test106ServerLocLessThan(t *testing.T) {
 		s1.Id = "a"
 		s1.Rank = 1
 		cv.So(ServerLocLessThan(&s1, &s2), cv.ShouldBeTrue)
-
-		// we should sort
-		// sloc='{"serverId":"5GbhwhtX80BGp2WkidaPMI","host":"127.0.0.1","port":57655,"leader":true,"leaseExpires":"2017-02-13T05:51:46.165603433Z","rank":1}'
-		// as <
-		// '{"serverId":"","host":"","port":0,"leader":false,"leaseExpires":"0001-01-01T00:00:00Z","rank":0}'
-		// also.
-
 	})
 }
-
-/*
-func ServerLocLessThan(i, j *ServerLoc, now time.Time) bool {
-
-sloc='{"serverId":"5GbhwhtX80BGp2WkidaPMI","host":"127.0.0.1","port":57655,"leader":true,"leaseExpires":"2017-02-13T05:51:46.165603433Z","rank":1}'
- >=
- prev:'{"serverId":"","host":"","port":0,"leader":false,"leaseExpires":"0001-01-01T00:00:00Z","rank":0}'
-*/
