11de29c706db6ee5e980ad760f4a5f4cd6178c84
Jason E. Aten <j.e.aten@gmail.com>
2017-03-24 14:23:27 -0500

    104 BcastSet test green

--- peer/api.go
+++ peer/api.go
@@ -118,7 +118,62 @@ func (peer *Peer) BcastGet(key []byte, includeValue bool, timeout time.Duration)
 	return
 }
 func (peer *Peer) BcastSet(ki *KeyInv) error {
-	return nil
+
+	timeout := 10 * time.Second
+	peers, err := peer.GetPeerList(timeout)
+	if err != nil || peers == nil || len(peers.Members) <= 1 {
+		// no peers, just do the local get.
+		p("no peers in BcastGet!?")
+		return peer.LocalSet(ki)
+	}
+	numPeers := len(peers.Members)
+	p("BcastSet sees numPeers = %v", numPeers)
+
+	req := &BcastSetRequest{
+		Ki: ki,
+	}
+	mm, err := req.MarshalMsg(nil)
+	if err != nil {
+		return err
+	}
+
+	inbox := nats.NewInbox()
+	ch := make(chan *nats.Msg, RequestChanLen)
+
+	s, err := peer.nc.ChanSubscribe(inbox, ch)
+	if err != nil {
+		return err
+	}
+	defer s.Unsubscribe()
+
+	err = peer.nc.PublishRequest(peer.subjBcastSet, inbox, mm)
+	if err != nil {
+		return err
+	}
+	toCh := time.After(timeout)
+
+	errs := ""
+	for i := 0; i < numPeers; i++ {
+		select {
+		case <-toCh:
+			return ErrTimedOut
+		case reply := <-ch:
+			p("BcastSet got a reply, on i = %v", i)
+			var rep BcastSetReply
+			_, err := rep.UnmarshalMsg(reply.Data)
+			if err != nil {
+				errs += err.Error() + ";"
+			} else {
+				if rep.Err != "" {
+					errs += rep.Err + ";"
+				}
+			}
+		}
+	}
+	if errs == "" {
+		return nil
+	}
+	return fmt.Errorf(errs)
 }
 
 func (peer *Peer) LocalGet(key []byte, includeValue bool) (ki *KeyInv, err error) {
