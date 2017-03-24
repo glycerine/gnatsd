1d39464d72401329ae328d0014c6a3b9a068a39b
Jason E. Aten <j.e.aten@gmail.com>
2017-03-24 00:26:19 -0500

    ft. 101 in peer for api ops

---
+++ peer/api.go
@@ -0,0 +1,26 @@
+package peer
+
+import (
+	"time"
+)
+
+func (peer *Peer) BcastKeyVal(key []byte, data []byte, tm time.Time) error {
+	return nil
+}
+
+func (peer *Peer) LocalGetKeyVal(key []byte) (data []byte, tm time.Time, err error) {
+	return
+}
+
+func (peer *Peer) StoreLocalKeyVal(key, val []byte, tm time.Time) error {
+
+	return nil
+}
+
+func (peer *Peer) GetLatest(key []byte) (val []byte, tm time.Time, err error) {
+	return
+}
+
+func (peer *Peer) GetLocal(key []byte) (val []byte, tm time.Time, err error) {
+	return
+}
--- peer/peer.go
+++ peer/peer.go
@@ -68,27 +68,6 @@ type Peer struct {
 	saver      Saver
 }
 
-func (peer *Peer) BcastKeyVal(key []byte, data []byte, tm time.Time) error {
-	return nil
-}
-
-func (peer *Peer) LocalGetKeyVal(key []byte) (data []byte, tm time.Time, err error) {
-	return
-}
-
-func (peer *Peer) StoreLocalKeyVal(key, val []byte, tm time.Time) error {
-
-	return nil
-}
-
-func (peer *Peer) GetLatest(key []byte) (val []byte, tm time.Time, err error) {
-	return
-}
-
-func (peer *Peer) GetLocal(key []byte) (val []byte, tm time.Time, err error) {
-	return
-}
-
 type leadFlag struct {
 	amLead bool
 	cs     clusterStatus
