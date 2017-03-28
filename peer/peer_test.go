465df7f0c7e56f76d7ad9d6dfaec438146bfdfef
Jason E. Aten <j.e.aten@gmail.com>
2017-03-28 18:47:49 -0500

    working on 104

--- peer/peer_test.go
+++ peer/peer_test.go
@@ -193,114 +193,109 @@ func Test102LocalSet(t *testing.T) {
 	})
 }
 
+func testSetupThree() (p0, p1, p2 *Peer, peers *LeadAndFollowList) {
+	cleanupTestUserDatabases()
+
+	u := newTestSshUser()
+
+	nPeerPort0, lsn0 := getAvailPort()
+	nPeerPort1, lsn1 := getAvailPort()
+	nPeerPort2, lsn2 := getAvailPort()
+	nPeerPort3, lsn3 := getAvailPort()
+	nPeerPort4, lsn4 := getAvailPort()
+	nPeerPort5, lsn5 := getAvailPort()
+
+	// don't close until now. Now we have non-overlapping ports.
+	lsn0.Close()
+	lsn1.Close()
+	lsn2.Close()
+	lsn3.Close()
+	lsn4.Close()
+	lsn5.Close()
+
+	cluster0 := fmt.Sprintf("-cluster=nats://localhost:%v", nPeerPort2)
+	cluster1 := fmt.Sprintf("-cluster=nats://localhost:%v", nPeerPort3)
+	cluster2 := fmt.Sprintf("-cluster=nats://localhost:%v", nPeerPort4)
+	routes1 := fmt.Sprintf("-routes=nats://localhost:%v", nPeerPort2)
+
+	// want peer0 to be lead, so we give it lower rank.
+	peer0cfg := strings.Join([]string{"-rank=0", "-health", "-p", fmt.Sprintf("%v", nPeerPort0), cluster0}, " ")
+
+	peer1cfg := strings.Join([]string{"-rank=3", "-health", "-p", fmt.Sprintf("%v", nPeerPort1), cluster1, routes1}, " ")
+
+	peer2cfg := strings.Join([]string{"-rank=6", "-health", "-p", fmt.Sprintf("%v", nPeerPort5), cluster2, routes1}, " ")
+	var err error
+	p0, err = NewPeer(peer0cfg, "p0")
+	panicOn(err)
+	p1, err = NewPeer(peer1cfg, "p1")
+	panicOn(err)
+	p2, err = NewPeer(peer2cfg, "p2")
+	panicOn(err)
+
+	p0.SshClientAllowsNewSshdServer = true
+	p1.SshClientAllowsNewSshdServer = true
+	p2.SshClientAllowsNewSshdServer = true
+
+	p0.TestAllowOneshotConnect = true
+	p1.TestAllowOneshotConnect = true
+	p2.TestAllowOneshotConnect = true
+
+	// start em up
+	err = p0.Start()
+	panicOn(err)
+	err = p1.Start()
+	panicOn(err)
+	err = p2.Start()
+	panicOn(err)
+
+	// add account to all peers, once their sshd is ready
+	<-p0.SshdReady
+	// 1st time sets u.rsaPath, whill will be re-used here-after.
+	creds0, err := u.addUserToSshd(p0.GservCfg.SshegoCfg)
+	panicOn(err)
+	p("creds0=%#v", creds0)
+
+	<-p1.SshdReady
+	creds1, err := u.addUserToSshd(p1.GservCfg.SshegoCfg)
+	panicOn(err)
+	p("creds1=%#v", creds1)
+
+	<-p2.SshdReady
+	creds2, err := u.addUserToSshd(p2.GservCfg.SshegoCfg)
+	panicOn(err)
+	p("creds2=%#v", creds2)
+
+	p0.SshClientLoginUsername = u.mylogin
+	p0.SshClientPrivateKeyPath = u.rsaPath
+	p0.SshClientClientKnownHostsPath = "p0.sshcli.known.hosts"
+
+	p1.SshClientLoginUsername = u.mylogin
+	p1.SshClientPrivateKeyPath = u.rsaPath
+	p1.SshClientClientKnownHostsPath = "p1.sshcli.known.hosts"
+
+	p2.SshClientLoginUsername = u.mylogin
+	p2.SshClientPrivateKeyPath = u.rsaPath
+	p2.SshClientClientKnownHostsPath = "p2.sshcli.known.hosts"
+
+	// let peers come up and start talking
+	peers, err = p0.WaitForPeerCount(3, 120*time.Second)
+	if err != nil || peers == nil || len(peers.Members) != 3 {
+		p("peers = %#v", peers)
+		panic(fmt.Sprintf("could not setup all 3 peers?!?: err = '%v'. ", err))
+	}
+
+	return p0, p1, p2, peers
+}
+
 func Test103BcastGet(t *testing.T) {
 
 	cv.Convey("Given three peers p0, p1, and p2, BcastGet should return KeyInv from both", t, func() {
 
-		cleanupTestUserDatabases()
-		defer cleanupTestUserDatabases()
-
-		u := newTestSshUser()
-
-		nPeerPort0, lsn0 := getAvailPort()
-		nPeerPort1, lsn1 := getAvailPort()
-		nPeerPort2, lsn2 := getAvailPort()
-		nPeerPort3, lsn3 := getAvailPort()
-		nPeerPort4, lsn4 := getAvailPort()
-		nPeerPort5, lsn5 := getAvailPort()
-
-		// don't close until now. Now we have non-overlapping ports.
-		lsn0.Close()
-		lsn1.Close()
-		lsn2.Close()
-		lsn3.Close()
-		lsn4.Close()
-		lsn5.Close()
-
-		cluster0 := fmt.Sprintf("-cluster=nats://localhost:%v", nPeerPort2)
-		cluster1 := fmt.Sprintf("-cluster=nats://localhost:%v", nPeerPort3)
-		cluster2 := fmt.Sprintf("-cluster=nats://localhost:%v", nPeerPort4)
-		routes1 := fmt.Sprintf("-routes=nats://localhost:%v", nPeerPort2)
-
-		// want peer0 to be lead, so we give it lower rank.
-		peer0cfg := strings.Join([]string{"-rank=0", "-health", "-p", fmt.Sprintf("%v", nPeerPort0), cluster0}, " ")
-
-		peer1cfg := strings.Join([]string{"-rank=3", "-health", "-p", fmt.Sprintf("%v", nPeerPort1), cluster1, routes1}, " ")
-
-		peer2cfg := strings.Join([]string{"-rank=6", "-health", "-p", fmt.Sprintf("%v", nPeerPort5), cluster2, routes1}, " ")
-
-		p0, err := NewPeer(peer0cfg, "p0")
-		panicOn(err)
-		p1, err := NewPeer(peer1cfg, "p1")
-		panicOn(err)
-		p2, err := NewPeer(peer2cfg, "p2")
-		panicOn(err)
-
-		p0.SshClientAllowsNewSshdServer = true
-		p1.SshClientAllowsNewSshdServer = true
-		p2.SshClientAllowsNewSshdServer = true
-
-		p0.TestAllowOneshotConnect = true
-		p1.TestAllowOneshotConnect = true
-		p2.TestAllowOneshotConnect = true
-
-		// start em up
-		err = p0.Start()
-		panicOn(err)
-		err = p1.Start()
-		panicOn(err)
-		err = p2.Start()
-		panicOn(err)
-
+		p0, p1, p2, _ := testSetupThree()
 		defer p0.Stop()
 		defer p1.Stop()
 		defer p2.Stop()
-
-		// add account to all peers, once their sshd is ready
-		<-p0.SshdReady
-		// 1st time sets u.rsaPath, whill will be re-used here-after.
-		creds0, err := u.addUserToSshd(p0.GservCfg.SshegoCfg)
-		panicOn(err)
-		p("creds0=%#v", creds0)
-
-		<-p1.SshdReady
-		creds1, err := u.addUserToSshd(p1.GservCfg.SshegoCfg)
-		panicOn(err)
-		p("creds1=%#v", creds1)
-
-		<-p2.SshdReady
-		creds2, err := u.addUserToSshd(p2.GservCfg.SshegoCfg)
-		panicOn(err)
-		p("creds2=%#v", creds2)
-
-		p0.SshClientLoginUsername = u.mylogin
-		p0.SshClientPrivateKeyPath = u.rsaPath
-		p0.SshClientClientKnownHostsPath = "p0.sshcli.known.hosts"
-
-		p1.SshClientLoginUsername = u.mylogin
-		p1.SshClientPrivateKeyPath = u.rsaPath
-		p1.SshClientClientKnownHostsPath = "p1.sshcli.known.hosts"
-
-		p2.SshClientLoginUsername = u.mylogin
-		p2.SshClientPrivateKeyPath = u.rsaPath
-		p2.SshClientClientKnownHostsPath = "p2.sshcli.known.hosts"
-
-		// set to allow new hosts (turn off MITM protection)
-		// Have to wait until sshd is ready to set this.
-		//		p0.GservCfg.SshegoCfg.TestAllowOneshotConnect = true
-		//		p1.GservCfg.SshegoCfg.TestAllowOneshotConnect = true
-		//		p2.GservCfg.SshegoCfg.TestAllowOneshotConnect = true
-
-		// let peers come up and start talking
-		peers, err := p0.WaitForPeerCount(3, 120*time.Second)
-		if err != nil || peers == nil || len(peers.Members) != 3 {
-			p("peers = %#v", peers)
-			panic(fmt.Sprintf("could not setup all 3 peers?!?: err = '%v'. ", err))
-		}
-
-		defer os.Remove("p0.boltdb")
-		defer os.Remove("p1.boltdb")
-		defer os.Remove("p2.boltdb")
+		defer cleanupTestUserDatabases()
 
 		t3 := time.Now().UTC()
 		t2 := t3.Add(-time.Minute)
@@ -313,7 +308,7 @@ func Test103BcastGet(t *testing.T) {
 
 		key := []byte("chk")
 
-		err = p0.LocalSet(&api.KeyInv{Key: key, Val: data0, When: t0})
+		err := p0.LocalSet(&api.KeyInv{Key: key, Val: data0, When: t0})
 		panicOn(err)
 		err = p1.LocalSet(&api.KeyInv{Key: key, Val: data1, When: t1})
 		panicOn(err)
@@ -349,65 +344,69 @@ func Test104BcastSet(t *testing.T) {
 
 	cv.Convey("Given three peers p0, p1, and p2, BcastSet should set the broadcast value on on all peers", t, func() {
 
-		nPeerPort0, lsn0 := getAvailPort()
-		nPeerPort1, lsn1 := getAvailPort()
-		nPeerPort2, lsn2 := getAvailPort()
-		nPeerPort3, lsn3 := getAvailPort()
-		nPeerPort4, lsn4 := getAvailPort()
-		nPeerPort5, lsn5 := getAvailPort()
-
-		// don't close until now. Now we have non-overlapping ports.
-		lsn0.Close()
-		lsn1.Close()
-		lsn2.Close()
-		lsn3.Close()
-		lsn4.Close()
-		lsn5.Close()
-
-		cluster0 := fmt.Sprintf("-cluster=nats://localhost:%v", nPeerPort2)
-		cluster1 := fmt.Sprintf("-cluster=nats://localhost:%v", nPeerPort3)
-		cluster2 := fmt.Sprintf("-cluster=nats://localhost:%v", nPeerPort4)
-		routes1 := fmt.Sprintf("-routes=nats://localhost:%v", nPeerPort2)
-
-		// want peer0 to be lead, so we give it lower rank.
-		peer0cfg := strings.Join([]string{"-rank=0", "-health", "-p", fmt.Sprintf("%v", nPeerPort0), cluster0}, " ")
-
-		peer1cfg := strings.Join([]string{"-rank=3", "-health", "-p", fmt.Sprintf("%v", nPeerPort1), cluster1, routes1}, " ")
-
-		peer2cfg := strings.Join([]string{"-rank=6", "-health", "-p", fmt.Sprintf("%v", nPeerPort5), cluster2, routes1}, " ")
-
-		p0, err := NewPeer(peer0cfg, "p0")
-		panicOn(err)
-		p1, err := NewPeer(peer1cfg, "p1")
-		panicOn(err)
-		p2, err := NewPeer(peer2cfg, "p2")
-		panicOn(err)
-
-		// start em up
-		err = p0.Start()
-		panicOn(err)
-		err = p1.Start()
-		panicOn(err)
-		err = p2.Start()
-		panicOn(err)
-
+		p0, p1, p2, _ := testSetupThree()
 		defer p0.Stop()
 		defer p1.Stop()
 		defer p2.Stop()
+		defer cleanupTestUserDatabases()
 
-		// let peers come up and start talking
-		peers, err := p0.WaitForPeerCount(3, 120*time.Second)
-		p("peers = %#v", peers)
-		if err != nil {
-			panic(fmt.Sprintf("could not setup all 3 peers?!?: err = '%v'. ", err))
-		}
-		if len(peers.Members) != 3 {
-			panic(fmt.Sprintf("could not setup all 3 peers??? count=%v. ", len(peers.Members)))
-		}
+		/*
+			nPeerPort0, lsn0 := getAvailPort()
+			nPeerPort1, lsn1 := getAvailPort()
+			nPeerPort2, lsn2 := getAvailPort()
+			nPeerPort3, lsn3 := getAvailPort()
+			nPeerPort4, lsn4 := getAvailPort()
+			nPeerPort5, lsn5 := getAvailPort()
 
-		defer os.Remove("p0.boltdb")
-		defer os.Remove("p1.boltdb")
-		defer os.Remove("p2.boltdb")
+			// don't close until now. Now we have non-overlapping ports.
+			lsn0.Close()
+			lsn1.Close()
+			lsn2.Close()
+			lsn3.Close()
+			lsn4.Close()
+			lsn5.Close()
+
+			cluster0 := fmt.Sprintf("-cluster=nats://localhost:%v", nPeerPort2)
+			cluster1 := fmt.Sprintf("-cluster=nats://localhost:%v", nPeerPort3)
+			cluster2 := fmt.Sprintf("-cluster=nats://localhost:%v", nPeerPort4)
+			routes1 := fmt.Sprintf("-routes=nats://localhost:%v", nPeerPort2)
+
+			// want peer0 to be lead, so we give it lower rank.
+			peer0cfg := strings.Join([]string{"-rank=0", "-health", "-p", fmt.Sprintf("%v", nPeerPort0), cluster0}, " ")
+
+			peer1cfg := strings.Join([]string{"-rank=3", "-health", "-p", fmt.Sprintf("%v", nPeerPort1), cluster1, routes1}, " ")
+
+			peer2cfg := strings.Join([]string{"-rank=6", "-health", "-p", fmt.Sprintf("%v", nPeerPort5), cluster2, routes1}, " ")
+
+			p0, err := NewPeer(peer0cfg, "p0")
+			panicOn(err)
+			p1, err := NewPeer(peer1cfg, "p1")
+			panicOn(err)
+			p2, err := NewPeer(peer2cfg, "p2")
+			panicOn(err)
+
+			// start em up
+			err = p0.Start()
+			panicOn(err)
+			err = p1.Start()
+			panicOn(err)
+			err = p2.Start()
+			panicOn(err)
+
+			defer p0.Stop()
+			defer p1.Stop()
+			defer p2.Stop()
+
+			// let peers come up and start talking
+			peers, err := p0.WaitForPeerCount(3, 120*time.Second)
+			p("peers = %#v", peers)
+			if err != nil {
+				panic(fmt.Sprintf("could not setup all 3 peers?!?: err = '%v'. ", err))
+			}
+			if len(peers.Members) != 3 {
+				panic(fmt.Sprintf("could not setup all 3 peers??? count=%v. ", len(peers.Members)))
+			}
+		*/
 
 		t3 := time.Now().UTC()
 		t2 := t3.Add(-time.Minute)
@@ -421,7 +420,7 @@ func Test104BcastSet(t *testing.T) {
 
 		key := []byte("chk")
 
-		err = p0.LocalSet(&api.KeyInv{Key: key, Val: data0, When: t0})
+		err := p0.LocalSet(&api.KeyInv{Key: key, Val: data0, When: t0})
 		panicOn(err)
 		err = p1.LocalSet(&api.KeyInv{Key: key, Val: data1, When: t1})
 		panicOn(err)
